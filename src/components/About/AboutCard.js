2c70031cc6ab8ecd88a12ae2cda0543f051c1346
Parvati Jayakumar <51737416+parvatijay2901@users.noreply.github.com>
2024-07-07 23:23:36 -0700

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -7,9 +7,9 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            I am currently a Data Science graduate student at the <span className="purple">University of Washington</span>. 
+            I am currently a Data Science graduate student at the <span className="purple">University of Washington</span>, work part time at the UW Laboratory for Auditory Neuroscience and Development and am interning at <span className="purple">Pearson Packaging Systems</span> this summer. 
             Proficient in Python, MATLAB, SQL, and various technologies, I specialize in <span className="purple">Data Analysis, Signal Processing, and Natural Language Processing. </span>
-            previously, As a Clinical Data Analyst at MiiCare UK, I managed NLP projects, led the design of the 'MiiVoice' MVP for mental health analysis, and researched Acoustic Gait Analysis.  
+            As a Clinical Data Analyst at MiiCare UK, I managed NLP projects, led the design of the 'MiiVoice' MVP for mental health analysis, and researched Acoustic Gait Analysis.  
             As a Microsoft Azure Machine Learning Scholar, I completed Udacity’s Phase 2 nanodegree with projects on Azure SDK. 
             I hold a <span className="purple">B.Tech. degree in Electronics and Communication</span>, receiving Institute and Department Gold Medals. 
             My projects showcase expertise in Acoustic Signal Processing, Data Analytics, and Basic Computer Vision. 
