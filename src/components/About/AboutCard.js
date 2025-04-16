d8262fe73905017903b5b5538f13522db6d9cc5d
Parvati Jayakumar <51737416+parvatijay2901@users.noreply.github.com>
2025-04-15 18:08:28 -0700

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -7,12 +7,11 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            I am currently a Data Science graduate student at the <span className="purple">University of Washington</span>, work part-time at the <span className="purple">UW Laboratory for Auditory Neuroscience and Development</span> and am interning at <span className="purple">Pearson Packaging Systems</span> this summer. 
-            Proficient in Python, MATLAB, SQL, and various technologies, I specialize in <span className="purple">Data Analysis, Signal Processing, and Natural Language Processing. </span>
-            As a Clinical Data Analyst at MiiCare UK, I managed NLP projects, led the design of the 'MiiVoice' MVP for mental health analysis, and researched Acoustic Gait Analysis.  
-            As a Microsoft Azure Machine Learning Scholar, I completed Udacity’s Phase 2 nanodegree with projects on Azure SDK. 
+            I recently graduated from the Data Science Graduate program at the <span className="purple">University of Washington</span>, worked part-time at the <span className="purple">UW eScience Institute</span> and interned at <span className="purple">Pearson Packaging Systems</span> last summer. 
+            Proficient in Python, MATLAB, SQL, and various technologies, I specialize in <span className="purple">Data Science, Data Analytics, Signal Processing, and Natural Language Processing. </span>
+            As a Data Scientist (R&D) at MiiCare UK, I managed NLP projects, led the design of the 'MiiVoice' MVP for mental health analysis, and researched Acoustic Gait Analysis.
             I hold a <span className="purple">B.Tech. degree in Electronics and Communication</span>, receiving Institute and Department Gold Medals. 
-            My projects showcase expertise in Acoustic Signal Processing, Data Analytics, and Basic Computer Vision. 
+            My projects showcase expertise in Data Science, Acoustic Signal Processing, Data Analytics, and Computer Vision. 
           </p>
         </blockquote>
       </Card.Body>
